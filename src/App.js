e3af730ec63581bed19ed701f04b655158a2a092
Smarteq-Onur <onurkordogan@gmail.com>
2023-12-13 16:00:40 +0300

    beginner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css'
+import Test from './Test'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{process.env.NODE_ENV}</h3>
+      <img src='/logo192.png' alt=''/>
+      {/* <img src={logo} alt=''/> */}
+      <Test/>
     </div>
   );
 }

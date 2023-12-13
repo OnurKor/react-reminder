6e34ba2da8169ad15654c239f283e72542f701ce
Smarteq-Onur <onurkordogan@gmail.com>
2023-12-13 16:44:12 +0300

    module.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './style.css'
+// import logo from './logo.svg';
 import Test from './Test'
+import { Title } from './Components';
 
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <h3>{process.env.NODE_ENV}</h3>
+    <div className={styles.App}>
+      <Title>{process.env.NODE_ENV}</Title>
+      <Title theme="dark">{process.env.NODE_ENV}</Title>
       <img src='/logo192.png' alt=''/>
-      {/* <img src={logo} alt=''/> */}
       <Test/>
     </div>
   );

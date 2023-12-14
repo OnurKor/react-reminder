4c7c672b98780a279896f8cdd12610acd73903aa
Smarteq-Onur <onurkordogan@gmail.com>
2023-12-14 10:42:12 +0300

    sass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Test from './Test'
 import { Title } from './Components';
 import './tailwind.css'
+import './style.scss'
 import styles from './App.module.css'
 import Tailwind from './Tailwind';
 
@@ -11,6 +12,10 @@ function App() {
     <div className={styles.App}>
       <Title>{process.env.NODE_ENV}</Title>
       <Title theme="dark">{process.env.NODE_ENV}</Title>
+      <p className='env'>
+      {process.env.NODE_ENV}
+      <span>Test</span>
+      </p>
       <img src='/logo192.png' alt=''/>
       <Test/>
       <Tailwind/>
